c4ef8dcca8b108fd0aa821cdf01ba734c6cc7e8d
Daniela Cuba (PGR) <2223642c@student.gla.ac.uk>
2023-10-03 15:26:44 +0100

    load data

--- initial_london.R
+++ initial_london.R
@@ -16,3 +16,6 @@ lapply(libs,require,character.only=T)
 ### Load data ----
 
 
+load("C:/Users/2223642C/OneDrive - University of Glasgow/Documents/PhD/UK_airpollutionDATA/london_griddedxmonth_data.RData") # longitude, latitude, time
+load("C:/Users/2223642C/OneDrive - University of Glasgow/Documents/PhD/UK_airpollutionDATA/london_aurnlocs.RData")
+aurn.data <- read.csv("C:/Users/2223642C/OneDrive - University of Glasgow/Documents/PhD/UK_airpollutionDATA/AURN_PM2p5Monthly_MAX_2013-2023.csv")
